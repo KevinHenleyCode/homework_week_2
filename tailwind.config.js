e025fc873a401364dbaff575820c837ce4fd0bd2
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-06-22 16:35:22 -0500

    changed css framework from bootstrap to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screen: {
+      sm: "300px",
+      md: "720px",
+      lg: "1200px",
+      xl: "1920px",
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
